90628e1e5af5bedba70751c1259ec002cd7f32e4
tains83 <110202684+tains83@users.noreply.github.com>
2026-02-18 17:27:43 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -52,7 +52,7 @@ const ADMIN_PASSWORD = "SanRocco";
 const CATEGORIES = ["Ingresso", "Gloria", "Salmo", "Alleluia", "Offertorio", "Santo", "Comunione", "Finale", "Mariano", "Spirito Santo", "Altro"];
 
 // Percorso aggiornato dell'icona richiesta
-const LOGO_SRC = "/icon.jpeg";
+const LOGO_SRC = "/icon.jpg";
 
 export default function App() {
   const [user, setUser] = useState(null);
